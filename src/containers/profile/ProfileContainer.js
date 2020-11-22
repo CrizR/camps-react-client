7da0ed1bdfb9b305de22a2a1cebcf42975bdf337
arisa-s <arisashiraishi@outlook.com>
2020-11-22 16:39:34 -0500

    Update ProfileContainer

--- src/containers/profile/ProfileContainer.js
+++ src/containers/profile/ProfileContainer.js
@@ -1,126 +1,108 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./ProfileContainerStyle.css";
 import {
-    Button,
-    Divider,
-    Form,
-    Grid,
-    Segment,
-    Container,
-    Header,
-    Image,
-    Menu,
-    Input,
+  Button,
+  Grid,
+  Segment,
+  Container,
+  Header,
+  Image,
+  Menu,
+  Loader,
 } from "semantic-ui-react";
 import NavBarComponent from "../../components/navbar/NavBarComponent";
-import {connect} from "react-redux";
-import {useAuth0} from "@auth0/auth0-react";
+import { setCurrentUserAction } from "./../../actions/UserActions";
+import {
+  getTripsAction,
+  getOwnedTripsAction,
+} from "./../../actions/TripActions";
 
-//TODO: BUILD PAGE TO SHOWCASE PROFILE, SHOWS RESERVED CAMPSITES AND MANAGED CAMPSITES (Two separate components)
-const ProfileContainer = ({id}) => {
-    const {getAccessTokenSilently, user, isAuthenticated} = useAuth0();
-    const [activeItem, setActiveItem] = React.useState("saved");
-    const [editing, setEditing] = React.useState(false);
+//TODO: SHOWCASE TRIPS
+const ProfileContainer = () => {
+  const { getAccessTokenSilently, user, logout, isAuthenticated } = useAuth0();
+  const [activeItem, setActiveItem] = useState("trips");
 
-    useEffect(() => {
-        getAccessTokenSilently({
-            audience: process.env.REACT_APP_AUTH_AUDIENCE,
-        }).then((token) => {
-        });
-    }, []);
+  
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.UserReducer.user);
+  const trips = useSelector((state) => state.TripsReducer.trips);
+  const owned_trips = useSelector((state) => state.TripsReducer.ownedTrips);
+  const isLoading = true
+  //useSelector((state) => state.tripReducer.isTripsLoading);
 
-    return (
-        <>
-            <NavBarComponent/>
-            <div className="camps-profile row">
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      dispatch(setCurrentUserAction(user));
+      getAccessTokenSilently({
+        audience: process.env.REACT_APP_AUTH_AUDIENCE,
+      }).then((token) => {
+        getTripsAction(dispatch, user, token);
+        getOwnedTripsAction(dispatch, user, token);
+      });
+    }
+  }, [isAuthenticated, user]);
+
+  return (
+    currentUser && (
+      <>
+        <NavBarComponent />
+        <div className="camps-profile row">
+          <Container>
+            <Grid>
+              <Grid.Column width={6}>
                 <Container>
-                    <Grid>
-                        <Grid.Column width={6}>
-                            <Container>
-                                <Header as="h1">
-                                    <Image circular src={user.picture}/>
-                                    <Header.Content>
-                                        {user.name}
-                                        <Header.Subheader>{user.email}</Header.Subheader>
-                                    </Header.Content>
-                                </Header>
-                                {!editing && (
-                                    <Button
-                                        fluid
-                                        content="Edit Profile"
-                                        icon="edit"
-                                        size="small"
-                                        onClick={() => setEditing(true)}
-                                    />
-                                )}
-                                {editing && (
-                                    <Form>
-                                        <Form.TextArea label='About'/>
-                                        <Form.Field>
-                                            <label>Location</label>
-                                            <input placeholder='Location'/>
-                                        </Form.Field>
-                                        <Form.Field>
-                                            <label>Phone</label>
-                                            <input placeholder='Phone'/>
-                                        </Form.Field>
-                                        <Form.Field>
-                                            <label>First Name</label>
-                                            <input placeholder='First Name'/>
-                                        </Form.Field>
-                                        <Form.Field>
-                                            <label>Last Name</label>
-                                            <input placeholder='Last Name'/>
-                                        </Form.Field>
-
-
-                                        <div>
-                                            <Button floated="left"
-                                                    onClick={() => setEditing(false)}>Cancel</Button>
-                                            <Button floated="right" positive
-                                                    onClick={() => setEditing(false)}>
-                                                Save
-                                            </Button>
-                                        </div>
-                                    </Form>
-
-                                )}
-                            </Container>
-                        </Grid.Column>
-                        <Grid.Column width={10}>
-                            <div>
-                                <Menu tabular attached="top">
-                                    <Menu.Item
-                                        name="saved"
-                                        active={activeItem === "saved"}
-                                        onClick={() => setActiveItem("saved")}
-                                    />
-                                    <Menu.Item
-                                        name="manage"
-                                        active={activeItem === "manage"}
-                                        onClick={() => setActiveItem("manage")}
-                                    />
-                                </Menu>
-                                <Segment attached="bottom">
-                                    {activeItem === "saved" && <h1>list of liked campsite</h1>}
-                                    {activeItem === "manage" && <h1>list of owned campsite</h1>}
-                                </Segment>
-                            </div>
-                        </Grid.Column>
-                    </Grid>
+                  <Header as="h1">
+                    <Image circular src={currentUser.picture} />
+                    <Header.Content>
+                      {currentUser.fName}
+                      <Header.Subheader>{currentUser.email}</Header.Subheader>
+                    </Header.Content>
+                  </Header>
+                  <Button
+                    fluid
+                    content="Logout"
+                    icon="external square alternate"
+                    size="small"
+                    onClick={() => logout()}
+                  />
                 </Container>
-            </div>
-        </>
-    );
+              </Grid.Column>
+              <Grid.Column width={10}>
+                <div>
+                  <Menu tabular attached="top">
+                    <Menu.Item
+                      name="Upcoming trips"
+                      active={activeItem === "trips"}
+                      onClick={() => setActiveItem("trips")}
+                    />
+                    <Menu.Item
+                      name="manage trips"
+                      active={activeItem === "manage"}
+                      onClick={() => setActiveItem("manage")}
+                    />
+                  </Menu>
+                  <Segment attached="bottom">
+                    {isLoading ? (
+                      <Loader active inline="centered">
+                        Loading
+                      </Loader>
+                    ) : (
+                      <div>
+                        {activeItem === "trips" && <div></div>}
+                        {activeItem === "manage" && <div></div>}
+                      </div>
+                    )}
+                  </Segment>
+                </div>
+              </Grid.Column>
+            </Grid>
+          </Container>
+        </div>
+      </>
+    )
+  );
 };
 
-const stateToProperty = (state) => ({
-    filtered: state.CampsiteReducer.filtered,
-});
-
-const propertyToDispatchMapper = (dispatch) => ({});
-
-export default connect(
-    stateToProperty,
-    propertyToDispatchMapper
-)(ProfileContainer);
+export default ProfileContainer;
