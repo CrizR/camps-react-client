35fc380cfcaeefffba7dceda0b077f886a91f4ff
arisa-s <arisashiraishi@outlook.com>
2020-11-22 16:39:08 -0500

    Update TripActions

--- src/actions/TripActions.js
+++ src/actions/TripActions.js
@@ -7,6 +7,17 @@ export const GET_TRIPS = "GET_TRIPS";
 export const SEARCH_TRIPS = "SEARCH_TRIPS";
 export const SELECT_TRIP = "SELECT_TRIP";
 export const GET_OWNED_TRIPS = "GET_OWNED_TRIPS";
+export const GET_TRIPS_PENDING = "GET_TRIPS_PENDING";
+export const GET_TRIPS_FAILURE = "GET_TRIPS_FAILURE";
+
+const setTripsPending = () => ({
+    type: GET_TRIPS_PENDING,
+});
+  
+const setTripsFailure = (error) => ({
+    type: GET_TRIPS_FAILURE,
+    error: error ? error.toString : "Problemo",
+});
 
 export function createTripAction(dispatch, user, tripObj, token) {
     return createTrip(user, tripObj, token).then(trip => {
@@ -37,22 +48,32 @@ export function deleteTripAction(dispatch, user, id, token) {
 }
 
 export function getTripsAction(dispatch, user, token) {
-    return getTrips(user, token).then(tripzes => {
-        return dispatch({
-            type: GET_TRIPS,
-            trips: tripzes
-        });
-    })
+    try {
+        dispatch(setTripsPending());
+        getTrips(user, token).then(tripzes => {
+            return dispatch({
+                type: GET_TRIPS,
+                trips: tripzes
+            });
+        })
+    } catch (err) {
+        setTripsFailure(err);
+    }
 }
 
 
 export function getOwnedTripsAction(dispatch, user, token) {
-    return getOwnedTrips(user, token).then(trips => {
-        return dispatch({
-            type: GET_OWNED_TRIPS,
-            trips: trips
-        });
-    })
+    try {
+        dispatch(setTripsPending());
+        getOwnedTrips(user, token).then(trips => {
+            return dispatch({
+                type: GET_OWNED_TRIPS,
+                trips: trips
+            });
+        })
+    } catch (err) {
+        setTripsFailure(err);
+    }  
 }
 
 export function selectTripAction(dispatch, id) {
