83e64cfb8a4ed3fbc0df3b7bafa93000bcf081a8
arisa-s <arisashiraishi@outlook.com>
2020-12-05 23:45:48 -0500

    Splitting App/Router

--- src/App.js
+++ src/App.js
@@ -1,79 +1,40 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import './App.css';
-import 'semantic-ui-css/semantic.min.css'
-import {combineReducers, createStore} from "redux";
-import {Provider} from "react-redux";
-import {useAuth0} from "@auth0/auth0-react";
+import React from "react";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
 import DashboardReducer from "./reducers/DashboardReducer";
-import UserReducer from "./reducers/UserReducer";
-import ProtectedRoute from "./auth/ProtectedRoute";
+import UserReducer from "./reducers/CurrentUserReducer";
 import LoadingComponent from "./components/loading/LoadingComponent";
-import HttpsRedirect from 'react-https-redirect';
-import DashboardContainer from "./containers/dashboard/DashboardContainer";
+import HttpsRedirect from "react-https-redirect";
 import TripsReducer from "./reducers/TripsReducer";
-import ProfileContainer from "./containers/profile/ProfileContainer";
-import CampgroundContainer from "./containers/campground/CampgroundContainer";
-import Home from "./containers/home/Home";
-import {createUserIfNotExist} from "./services/UserService";
-import {setCurrentUserAction} from "./actions/UserActions";
-
+import AppRouter from "./Router";
 
 const rootReducer = combineReducers({
-    DashboardReducer,
-    UserReducer,
-    TripsReducer
+  DashboardReducer,
+  UserReducer,
+  TripsReducer,
 });
 
-const store = createStore(
-    rootReducer,
-);
-
+const store = createStore(rootReducer);
 
 function App() {
-    const {isLoading, isAuthenticated, user, getAccessTokenSilently} = useAuth0();
+  const { isLoading } = useAuth0();
 
-    if (isLoading) {
-        return <LoadingComponent/>
-    }
+  if (isLoading) {
+    return <LoadingComponent />;
+  }
 
-    if (isAuthenticated && !!user) {
-        getAccessTokenSilently({
-            audience: process.env.REACT_APP_AUTH_AUDIENCE,
-        }).then((token) => {
-            createUserIfNotExist(user, token)
-        })
-    }
-
-    return (
-        <div className="App ap">
-            <HttpsRedirect>
-                <Provider store={store}>
-                    <Router>
-                        {isAuthenticated ?
-                            <>
-                                <Route exact path="/" component={DashboardContainer}/>
-                                <ProtectedRoute path="/dashboard/" exact component={DashboardContainer}/>
-                                <ProtectedRoute path='/profile' exact component={ProfileContainer}/>
-                                <ProtectedRoute path='/campground/:id' exact component={(routerProps) =>
-                                    <CampgroundContainer id={routerProps.match.params.id}/>}/>
-                            </>
-                            :
-                            <>
-                                <Route exact path="/" component={Home}/>
-                                <Route path='/profile/:id' exact component={ProfileContainer}/>
-                                <Route path="/dashboard/" exact component={DashboardContainer}/>
-                                <Route path='/campground/:id' exact component={(routerProps) =>
-                                    <CampgroundContainer id={routerProps.match.params.id}/>}/>
-                            </>
-                        }
-
-
-                    </Router>
-                </Provider>
-            </HttpsRedirect>
-        </div>
-    );
+  return (
+    <div className="App ap">
+      <HttpsRedirect>
+        <Provider store={store}>
+          <AppRouter />
+        </Provider>
+      </HttpsRedirect>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Router.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import DashboardContainer from "./containers/dashboard/DashboardContainer";
+import ProfileContainer from "./containers/profile/ProfileContainer";
+import CampgroundContainer from "./containers/campground/CampgroundContainer";
+import Home from "./containers/home/Home";
+import { createUserIfNotExist } from "./services/UserService";
+import {
+  selectCurrentUser,
+  setCurrentUserAction,
+} from "./actions/CurrentUserActions";
+
+const AppRouter = () => {
+  const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
+  const dispatch = useDispatch();
+  const currentUser = useSelector(selectCurrentUser);
+
+  if (isAuthenticated && !!user && !currentUser) {
+    getAccessTokenSilently({
+      audience: process.env.REACT_APP_AUTH_AUDIENCE,
+    })
+      .then((token) => {
+        return createUserIfNotExist(user, token);
+      })
+      .then((user) => {
+        dispatch(setCurrentUserAction(user));
+      })
+      .catch((e) => {
+        debugger;
+      });
+  }
+
+  console.log({ currentUser });
+
+  return (
+    <Router>
+      {isAuthenticated ? (
+        <>
+          <Route exact path="/" component={DashboardContainer} />
+          <ProtectedRoute
+            path="/dashboard/"
+            exact
+            component={DashboardContainer}
+          />
+          <ProtectedRoute path="/profile" exact component={ProfileContainer} />
+          <ProtectedRoute
+            path="/campground/:id"
+            exact
+            component={(routerProps) => (
+              <CampgroundContainer id={routerProps.match.params.id} />
+            )}
+          />
+        </>
+      ) : (
+        <>
+          <Route exact path="/" component={Home} />
+          <Route path="/profile/:id" exact component={ProfileContainer} />
+          <Route path="/dashboard/" exact component={DashboardContainer} />
+          <Route
+            path="/campground/:id"
+            exact
+            component={(routerProps) => (
+              <CampgroundContainer id={routerProps.match.params.id} />
+            )}
+          />
+        </>
+      )}
+    </Router>
+  );
+};
+
+export default AppRouter;
