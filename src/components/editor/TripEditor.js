17bfe629c0e0b2183d09cddcc2cf6c4afa117943
arisa-s <arisashiraishi@outlook.com>
2020-12-09 22:01:44 -0500

    Add 'add trip' functionality

--- src/components/editor/TripEditor.js
+++ src/components/editor/TripEditor.js
@@ -1,89 +1,116 @@
-import React, {useEffect, useState} from 'react';
-import "./TripEditorStyle.css"
-import {
-    Button,
-    Form, Input,
-    Modal, TextArea,
-} from "semantic-ui-react";
-import {connect} from "react-redux";
-import {useAuth0} from "@auth0/auth0-react";
+import React, { useEffect, useState } from "react";
+import "./TripEditorStyle.css";
+import { Button, Form, Input, Modal, TextArea } from "semantic-ui-react";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
+import { createTripAction, updateTripAction } from "../../actions/TripActions";
+import { selectCurrentUser } from "../../actions/CurrentUserActions";
 
 // TODO Change this whole thing to not use JSON and to allow you to create a campsite
-const TripEditor = ({campsite, triggerElement, isEdit}) => {
+const TripEditor = ({ campsite, triggerElement, isEdit, existingTrip }) => {
+  const dispatch = useDispatch();
 
+  const [open, setOpen] = React.useState(false);
+  const { getAccessTokenSilently } = useAuth0();
+  const currentUser = useSelector(selectCurrentUser);
+  const [editStateTrip, setEditStateTrip] = useState(
+    existingTrip || {
+      name: "",
+      inviteList: "",
+      description: "",
+      date: new Date().valueOf(),
+      campsite,
+    }
+  );
 
-    const [open, setOpen] = React.useState(false);
-    const {getAccessTokenSilently, user} = useAuth0();
-    const [token, setToken] = useState(undefined);
+  const updateField = (e, fieldName) =>
+    setEditStateTrip({
+      ...editStateTrip,
+      [fieldName]: fieldName === "date" ? e : e.target.value,
+    });
 
-    useEffect(() => {
-        getAccessTokenSilently({
-            audience: process.env.REACT_APP_AUTH_AUDIENCE,
-        }).then((token) => {
-            setToken(token)
-        });
-    }, []);
-
-
-    return (
-        <Modal
-            onClose={() => setOpen(false)}
-            onOpen={() => setOpen(true)}
-            open={open}
-            trigger={
-                triggerElement
-            }
+  return (
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={triggerElement}
+    >
+      <Modal.Header>
+        {isEdit ? <h4>Edit Trip</h4> : <h4>Create Trip</h4>}
+      </Modal.Header>
+      <Modal.Content className={"camps-json-input-comp"}>
+        <Form>
+          <Form.Group widths="equal">
+            <Form.Field
+              control={Input}
+              label="Trip Name"
+              placeholder="Default Trip"
+              onChange={(e) => updateField(e, "name")}
+              value={editStateTrip.name}
+            />
+          </Form.Group>
+          <Form.Field
+            control={Input}
+            type={"email"}
+            label="Invite List"
+            placeholder="abc@gmail.com"
+            onChange={(e) => updateField(e, "inviteList")}
+            value={editStateTrip.inviteList}
+          />
+          <Form.Field
+            control={TextArea}
+            label="Trip Description"
+            placeholder="..."
+            onChange={(e) => updateField(e, "description")}
+            value={editStateTrip.description}
+          />
+          <Form.Field>
+            <label>Date</label>
+            <DatePicker
+              value={new Date(editStateTrip.date).toDateString()}
+              onChange={(e) => updateField(e.valueOf(), "date")}
+            />
+          </Form.Field>
+        </Form>
+      </Modal.Content>
+      <Modal.Actions className={"camps-create-campsite-card-actions"}>
+        <Button
+          onClick={() => {
+            if (!currentUser) return;
+            getAccessTokenSilently({
+              audience: process.env.REACT_APP_AUTH_AUDIENCE,
+            }).then((token) => {
+              isEdit
+                ? updateTripAction(
+                    dispatch,
+                    currentUser,
+                    existingTrip.id,
+                    { trip: editStateTrip },
+                    token
+                  )
+                : createTripAction(
+                    dispatch,
+                    currentUser,
+                    { trip: editStateTrip },
+                    token
+                  );
+            });
+          }}
         >
-            <Modal.Header>
-                {isEdit ?
-                    <h4>Edit Trip</h4>
-                    :
-                    <h4>Create Trip</h4>
-                }
-            </Modal.Header>
-            <Modal.Content className={'camps-json-input-comp'}>
-                <Form>
-                    <Form.Group widths='equal'>
-                        <Form.Field
-                            control={Input}
-                            label='Trip Name'
-                            placeholder='Default Trip'
-                        />
-                    </Form.Group>
-                    <Form.Field
-                        control={Input}
-                        type={"email"}
-                        label='Invite List'
-                        placeholder='abc@gmail.com'
-                    />
-                    <Form.Field
-                        control={TextArea}
-                        label='Trip Description'
-                        placeholder='...'
-                    />
-                    <Form.Field>
-                        <label>Date</label>
-                        <DatePicker/>
-                    </Form.Field>
-
-                </Form>
-            </Modal.Content>
-            <Modal.Actions className={'camps-create-campsite-card-actions'}>
-                <Button>Save Trip</Button>
-            </Modal.Actions>
-        </Modal>
-    );
+          Save Trip
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
 };
 
 const stateToProperty = (state) => ({
-    selectedCampsite: state.DashboardReducer.selected
+  selectedCampsite: state.DashboardReducer.selected,
 });
 
 const propertyToDispatchMapper = (dispatch) => ({});
 
-export default connect
-(stateToProperty, propertyToDispatchMapper)
-(TripEditor)
+export default connect(stateToProperty, propertyToDispatchMapper)(TripEditor);
--- src/services/TripsService.js
+++ src/services/TripsService.js
@@ -1,91 +1,81 @@
 import Trip from "../model/TripModel";
 
-const apiUrl = process.env.NODE_ENV === 'production' ? "https://camps-node-server.herokuapp.com/api" : "http://localhost:8080/api";
-
+const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://camps-node-server.herokuapp.com/api"
+    : "http://localhost:8080/api";
 
 export const getTrips = (user, token) => {
-    return fetch(`${apiUrl}/user/${user.email}/trips`,
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                "Authorization": `Bearer ${token}`
-            }
-        })
-        .then(response => response.json())
-        .then(trips => {
-            return trips.map((trip) => Trip.fromStorage(trip))
-        });
+  return fetch(`${apiUrl}/user/${user.sub}/trips/owned`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((trips) => {
+      return trips.map((trip) => Trip.fromStorage(trip));
+    });
 };
 
 export const getOwnedTrips = (user, token) => {
-
-    return fetch(`${apiUrl}/user/${user.email}/trips/owned`,
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                "Authorization": `Bearer ${token}`
-            }
-        })
-        .then(response => response.json())
-        .then(trips => {
-            return trips.map((trip) => Trip.fromStorage(trip))
-        });
+  return fetch(`${apiUrl}/user/${user.email}/trips/owned`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((trips) => {
+      return trips.map((trip) => Trip.fromStorage(trip));
+    });
 };
-
 
 export const inviteToTrip = (user, email, tripId, token) => {
-
-    return fetch(`${apiUrl}/user/${user.sub}/trips/${tripId}/invite`,
-        {
-            method: 'POST',
-            body: JSON.stringify({email: email}),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                "Authorization": `Bearer ${token}`
-            }
-        })
-        .then(response => response.json())
+  return fetch(`${apiUrl}/user/${user.sub}/trips/${tripId}/invite`, {
+    method: "POST",
+    body: JSON.stringify({ email: email }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => response.json());
 };
 
-
 export const createTrip = (user, trip, token) =>
-    fetch(`${apiUrl}/user/${user.sub}/trip/`,
-        {
-            method: 'POST',
-            body: JSON.stringify(trip),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                "Authorization": `Bearer ${token}`
-            }
-        })
-        .then(response => response.json())
-        .then(trip => Trip.fromStorage(trip));
-
+  fetch(`${apiUrl}/user/${user.sub}/trips`, {
+    method: "POST",
+    body: JSON.stringify(trip),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((trip) => Trip.fromStorage(trip));
 
 export const updateTrip = (user, id, trip, token) =>
-    fetch(`${apiUrl}/user/${user.sub}/trip/${id}`,
-        {
-            method: 'PUT',
-            body: JSON.stringify(trip),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                "Authorization": `Bearer ${token}`
-            }
-        })
-        .then(response => response.json())
-        .then(trip => Trip.fromStorage(trip));
-
+  fetch(`${apiUrl}/user/${user.sub}/trip/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(trip),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((trip) => Trip.fromStorage(trip));
 
 export const deleteTrip = (user, id, token) =>
-    fetch(`${apiUrl}/user/${user.sub}/trip/${id}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            "Authorization": `Bearer ${token}`
-        }
-    });
+  fetch(`${apiUrl}/user/${user.sub}/trip/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
