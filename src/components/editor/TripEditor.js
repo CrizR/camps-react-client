dfa02304ff54df4e0af13119ec806f9ded12922d
Zain <reply2zain@gmail.com>
2020-12-13 16:14:33 -0500

    Added status for button

--- src/components/editor/TripEditor.js
+++ src/components/editor/TripEditor.js
@@ -1,36 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, {useState} from "react";
 import "./TripEditorStyle.css";
-import { Button, Form, Input, Modal, TextArea } from "semantic-ui-react";
-import { connect, useDispatch, useSelector } from "react-redux";
-import { useAuth0 } from "@auth0/auth0-react";
+import {Button, Form, Input, Modal, TextArea} from "semantic-ui-react";
+import {connect, useDispatch, useSelector} from "react-redux";
+import {useAuth0} from "@auth0/auth0-react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { createTripAction, updateTripAction } from "../../actions/TripActions";
-import { selectCurrentUser } from "../../actions/CurrentUserActions";
-import { inviteToTrip } from "../../services/TripsService";
+import {createTripAction, updateTripAction} from "../../actions/TripActions";
+import {selectCurrentUser} from "../../actions/CurrentUserActions";
+import {inviteToTrip} from "../../services/TripsService";
 
 // TODO Change this whole thing to not use JSON and to allow you to create a campsite
-const TripEditor = ({ campground, triggerElement, isEdit, existingTrip }) => {
+const TripEditor = ({campground, triggerElement, isEdit, existingTrip}) => {
   console.log(campground);
   const dispatch = useDispatch();
   const [open, setOpen] = React.useState(false);
-  const { getAccessTokenSilently } = useAuth0();
+  const {getAccessTokenSilently} = useAuth0();
   const currentUser = useSelector(selectCurrentUser);
   const [editStateTrip, setEditStateTrip] = useState(
-    existingTrip || {
-      name: "",
-      inviteList: "",
-      description: "",
-      date: new Date().valueOf(),
-      campground,
-    }
+      existingTrip || {
+        name: "",
+        inviteList: "",
+        description: "",
+        date: new Date().valueOf(),
+        campground,
+        buttonClicked: false
+      }
   );
 
   const updateField = (e, fieldName) =>
-    setEditStateTrip({
-      ...editStateTrip,
-      [fieldName]: fieldName === "date" ? e : e.target.value,
-    });
+      setEditStateTrip({
+        ...editStateTrip,
+        [fieldName]: fieldName === "date" ? e : e.target.value,
+        buttonClicked: false
+      });
+
+  const updateButtonClicked = (bVal) =>
+      setEditStateTrip({
+        ...editStateTrip,
+        buttonClicked: bVal
+      });
 
   const formatTrip = (trip) => ({
     ...trip,
@@ -38,99 +46,110 @@ const TripEditor = ({ campground, triggerElement, isEdit, existingTrip }) => {
   });
 
   return (
-    <Modal
-      onClose={() => setOpen(false)}
-      onOpen={() => setOpen(true)}
-      open={open}
-      trigger={triggerElement}
-    >
-      <Modal.Header>
-        {isEdit ? <h4>Edit Trip</h4> : <h4>Create Trip</h4>}
-      </Modal.Header>
-      <Modal.Content className={"camps-json-input-comp"}>
-        <Form>
-          <Form.Group widths="equal">
+      <Modal
+          onClose={() => setOpen(false)}
+          onOpen={() => setOpen(true)}
+          open={open}
+          trigger={triggerElement}
+      >
+        <Modal.Header>
+          {isEdit ? <h4>Edit Trip</h4> : <h4>Create Trip</h4>}
+        </Modal.Header>
+        <Modal.Content className={"camps-json-input-comp"}>
+          <Form>
+            <Form.Group widths="equal">
+              <Form.Field
+                  control={Input}
+                  label="Trip Name"
+                  placeholder="Default Trip"
+                  onChange={(e) => updateField(e, "name")}
+                  value={editStateTrip.name}
+              />
+            </Form.Group>
             <Form.Field
-              control={Input}
-              label="Trip Name"
-              placeholder="Default Trip"
-              onChange={(e) => updateField(e, "name")}
-              value={editStateTrip.name}
+                control={Input}
+                type={"email"}
+                label="Invite List"
+                placeholder="abc@gmail.com"
+                onChange={(e) => updateField(e, "inviteList")}
+                value={editStateTrip.inviteList}
             />
-          </Form.Group>
-          <Form.Field
-            control={Input}
-            type={"email"}
-            label="Invite List"
-            placeholder="abc@gmail.com"
-            onChange={(e) => updateField(e, "inviteList")}
-            value={editStateTrip.inviteList}
-          />
-          <Form.Field
-            control={TextArea}
-            label="Trip Description"
-            placeholder="..."
-            onChange={(e) => updateField(e, "description")}
-            value={editStateTrip.description}
-          />
-          <Form.Field>
-            <label>Date</label>
-            <DatePicker
-              value={new Date(editStateTrip.date).toDateString()}
-              onChange={(e) => updateField(e.valueOf(), "date")}
+            <Form.Field
+                control={TextArea}
+                label="Trip Description"
+                placeholder="..."
+                onChange={(e) => updateField(e, "description")}
+                value={editStateTrip.description}
             />
-          </Form.Field>
-        </Form>
-      </Modal.Content>
-      <Modal.Actions className={"camps-create-campsite-card-actions"}>
-        <Button
-          onClick={() => {
-            if (!currentUser) return;
+            <Form.Field>
+              <label>Date</label>
+              <DatePicker
+                  value={new Date(editStateTrip.date).toDateString()}
+                  onChange={(e) => updateField(e.valueOf(), "date")}
+              />
+            </Form.Field>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions className={"camps-create-campsite-card-actions"}>
+          {
+            editStateTrip.buttonClicked === false &&
+          <Button
+              onClick={() => {
+                if (!currentUser) {
+                  return;
+                }
+                getAccessTokenSilently({
+                  audience: process.env.REACT_APP_AUTH_AUDIENCE,
+                }).then((token) => {
+                  const formattedTrip = formatTrip(editStateTrip);
 
-            getAccessTokenSilently({
-              audience: process.env.REACT_APP_AUTH_AUDIENCE,
-            }).then((token) => {
-              const formattedTrip = formatTrip(editStateTrip);
-
-              isEdit
-                ? updateTripAction(
-                    dispatch,
-                    currentUser,
-                    existingTrip.id,
-                    { trip: formattedTrip },
-                    token
-                  ).then((trip) => {
-                    if (trip.inviteList.length) {
-                      // edit user trip
-                    }
-                  })
-                : createTripAction(
-                    dispatch,
-                    currentUser,
-                    { trip: formattedTrip },
-                    token
-                  ).then((trip) => {
-                    if (trip.inviteList.length) {
-                      // collection of async calls
-                      Promise.all(
-                        trip.inviteList.map((email) => {
-                          inviteToTrip(
-                            currentUser.sub,
-                            email,
-                            trip.id,
-                            token
-                          ).catch((e) => console.error(e));
-                        })
-                      ).then(() => console.log("success"));
-                    }
-                  });
-            });
-          }}
-        >
-          Save Trip
-        </Button>
-      </Modal.Actions>
-    </Modal>
+                  isEdit
+                      ? updateTripAction(
+                      dispatch,
+                      currentUser,
+                      existingTrip.id,
+                      {trip: formattedTrip},
+                      token
+                      ).then((trip) => {
+                        if (trip.inviteList.length) {
+                          // edit user trip
+                        }
+                      })
+                      : createTripAction(
+                      dispatch,
+                      currentUser,
+                      {trip: formattedTrip},
+                      token
+                      ).then((trip) => {
+                        if (trip.inviteList.length) {
+                          // collection of async calls
+                          Promise.all(
+                              trip.inviteList.map((email) => {
+                                inviteToTrip(
+                                    currentUser.sub,
+                                    email,
+                                    trip.id,
+                                    token
+                                ).catch((e) => console.error(e));
+                              })
+                          ).then(() => console.log("success"));
+                        }
+                      });
+                });
+                updateButtonClicked(true);
+              }}
+          >
+            Save Trip
+          </Button>
+          }
+          {
+            editStateTrip.buttonClicked === true &&
+                <Button basic color='green'>
+                  Saved!
+                </Button>
+          }
+        </Modal.Actions>
+      </Modal>
   );
 };
 
